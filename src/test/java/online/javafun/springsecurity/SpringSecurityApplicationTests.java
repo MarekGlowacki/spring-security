9c9f6925d651ffaac3307c476440dc4bb64ffca7
User <user@its.me>
2023-04-08 08:15:06 +0200

    Initial commit

---
+++ src/test/java/online/javafun/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
