e744bfa7ecf6c32b6fc1a87259595310322d2b1c
User <user@its.me>
2023-04-10 08:57:25 +0200

    reduce reduntant import

--- src/main/java/online/javafun/springsecurity/SecurityConfig.java
+++ src/main/java/online/javafun/springsecurity/SecurityConfig.java
@@ -1,6 +1,4 @@
 package online.javafun.springsecurity;
-
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
