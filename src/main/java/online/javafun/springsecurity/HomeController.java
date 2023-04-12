e2636307828193885f16d241908a938c18851309
User <user@its.me>
2023-04-12 11:05:25 +0200

    get username from service

--- src/main/java/online/javafun/springsecurity/HomeController.java
+++ src/main/java/online/javafun/springsecurity/HomeController.java
@@ -1,16 +1,25 @@
 package online.javafun.springsecurity;
 
-import org.springframework.security.core.annotation.CurrentSecurityContext;
+import online.javafun.message.WelcomeMessageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    
+
+    private final WelcomeMessageService welcomeMessageService;
+
+    public HomeController(WelcomeMessageService welcomeMessageService) {
+        this.welcomeMessageService = welcomeMessageService;
+    }
+
     @GetMapping("/")
-    String home(@CurrentSecurityContext(expression = "authentication.name") String username, Model model) {
-        model.addAttribute("username", username);
-        return "index";    
+    String home(@RequestParam(defaultValue = "en") String lang, Model model) {
+        model.addAttribute("lang", lang);
+        String welcomeMessage = welcomeMessageService.getWelcomeMessage(lang);
+        model.addAttribute("welcomeMessage", welcomeMessage);
+        return "index";
     }
 }
