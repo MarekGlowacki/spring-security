b443e209d70c73070fa9e914eecb173468a49364
User <user@its.me>
2023-04-12 11:07:23 +0200

    comment language change

--- src/main/java/online/javafun/message/WelcomeMessageService.java
+++ src/main/java/online/javafun/message/WelcomeMessageService.java
@@ -1,20 +1,19 @@
 package online.javafun.message;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WelcomeMessageService {
-    public static final String LANG_ES = "es";
-    public static final String LANG_PL = "pl";
-
-    public String getWelcomeMessage(String lang) {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        String welcomeMessage = switch (lang) {
-            case LANG_ES -> "Hola";
-            case LANG_PL -> "Cześć";
-            default -> "Hello";
-        };
-        return welcomeMessage + " " + username;
-    }
+//    public static final String LANG_ES = "es";
+//    public static final String LANG_PL = "pl";
+//
+//    public String getWelcomeMessage(String lang) {
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        String welcomeMessage = switch (lang) {
+//            case LANG_ES -> "Hola";
+//            case LANG_PL -> "Cześć";
+//            default -> "Hello";
+//        };
+//        return welcomeMessage + " " + username;
+//    }
 }
