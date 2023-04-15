b2bb75265daaaa16ee3e496e0eb1165d126c1056
User <user@its.me>
2023-04-15 16:31:59 +0200

    allow load css styles without log

--- src/main/java/online/javafun/config/SecurityConfig.java
+++ src/main/java/online/javafun/config/SecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests(
+                requests -> requests
+                        .requestMatchers("/img/**", "/styles/**").permitAll()
+                        .anyRequest().authenticated());
         http.formLogin(login -> login.loginPage("/login").permitAll());
         http.csrf().disable();
         return http.build();
