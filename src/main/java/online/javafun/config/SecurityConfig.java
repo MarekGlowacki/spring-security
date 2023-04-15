80f0871054e9ab55516d7bc0d6b476b03a6c00b4
User <user@its.me>
2023-04-15 16:04:36 +0200

    example user roles definition

--- src/main/java/online/javafun/config/SecurityConfig.java
+++ src/main/java/online/javafun/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package online.javafun.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,7 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests(requests -> requests
+                .mvcMatchers("/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user-panel/**").hasAuthority("ROLE_USER")
+                .mvcMatchers(HttpMethod.POST, "/calculate").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated();
+        );
         http.formLogin(login -> login.loginPage("/login").permitAll());
         http.csrf().disable();
         return http.build();
